336e10d59ccec3554d7d07bbe3f36b7186ff2a4a
Carlos Massa <carlos.massa.10@gmail.com>
2024-01-27 19:40:37 +0100

    Add vertex class

---
+++ include/vertex.h
@@ -0,0 +1,10 @@
+
+struct Vertex {
+    
+    Vertex(float x, float y, float z) {
+        c[0] = x;
+        c[1] = y;
+        c[2] = z;
+    }
+    float c[3] = {0.f, 0.f, 0.f};
+};
--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <GLFW/glfw3.h>
 #include <string>
 #include <map>
+#include <vector>
 
 #include "include/glfw_utilities.h"
 #include "include/image_utilities.h"
 #include "include/settings.h"
+#include "include/vertex.h"
 
 std::map<std::string, GLuint> textures;
 
@@ -18,15 +20,14 @@ void display(GLFWwindow* window) {
 
     glBindTexture(GL_TEXTURE_2D, textures.at("DVD_LOGO"));
 
-    GLfloat vertices[] = {
-        /*   position   */     /* texture */
-        -1.0f, -1.0f, 0.0f,     0.0f, 0.0f,
-         1.0f, -1.0f, 0.0f,     1.0f, 0.0f,
-         1.0f,  1.0f, 0.0f,     1.0f, 1.0f,
+    std::vector<Vertex> vertices = {
+        Vertex(-1.0f, -1.0f, 0.0f),
+        Vertex( 1.0f, -1.0f, 0.0f),
+        Vertex( 1.0f,  1.0f, 0.0f),
 
-        -1.0f, -1.0f, 0.0f,     0.0f, 0.0f,
-         1.0f,  1.0f, 0.0f,     1.0f, 1.0f,
-        -1.0f,  1.0f, 0.0f,     0.0f, 1.0f 
+        Vertex(-1.0f, -1.0f, 0.0f),
+        Vertex( 1.0f,  1.0f, 0.0f),
+        Vertex(-1.0f,  1.0f, 0.0f)
     };
     GLint elems[] = { 0, 1, 2, 0, 2, 3 };
 
@@ -40,7 +41,7 @@ void display(GLFWwindow* window) {
     // Setup VBO
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, 5 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 5 * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*) 0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (const GLvoid*) (3 * sizeof(GLfloat)));
